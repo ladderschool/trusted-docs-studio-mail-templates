0d754ad500451ed20606a563e580c44d0b092997
ladderschool <ladderschooledu@gmail.com>
2025-07-20 00:49:10 -0400

    vite config and possibleTypes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import type { ReactNode } from 'react'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
+import possibleTypes from 'src/graphql/possibleTypes'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
 import { AuthProvider, useAuth } from './auth'
@@ -18,7 +19,14 @@ const App = ({ children }: AppProps) => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
       <AuthProvider>
-        <RedwoodApolloProvider useAuth={useAuth}>
+        <RedwoodApolloProvider
+          useAuth={useAuth}
+          graphQLClientConfig={{
+            cacheConfig: {
+              ...possibleTypes,
+            },
+          }}
+        >
           {children}
         </RedwoodApolloProvider>
       </AuthProvider>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,10 @@
 import dns from 'dns'
+import path from 'path'
 
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
 
+import { getPaths } from '@redwoodjs/project-config'
 import redwood from '@redwoodjs/vite'
 
 // So that Vite will load on localhost instead of `127.0.0.1`.
@@ -11,6 +13,12 @@ dns.setDefaultResultOrder('verbatim')
 
 const viteConfig: UserConfig = {
   plugins: [redwood()],
+  resolve: {
+    alias: {
+      web: path.resolve(getPaths().web.base),
+      src: path.resolve(getPaths().web.src),
+    },
+  },
 }
 
 export default defineConfig(viteConfig)
